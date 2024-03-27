1b6ca54131f0ca32d50eb91aa3e0a5c1ca325929
RRachelvd <156345931+RRachelvd@users.noreply.github.com>
2024-03-27 18:44:01 +0100

    Updated css and AI integration

--- script.js
+++ script.js
@@ -13,11 +13,14 @@ function displayGeneratedPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
 
+  let poemHidden = document.querySelector("#poem");
+  poemHidden.classList.remove("hidden");
+
   let instructionsInput = document.querySelector("#instructions");
   let apiKey = "0c98c0be68f4tba31fe26f898obb603d";
   let prompt = `User instructions are: Generate a poem about ${instructionsInput.value} without any remarks`;
   let context =
-    "You are a romantic poem expert and love to write medium long poems. Your mission is to generate a 4, 6 or 8 line poem after every line add a < /br> in HTML format, do not show < /br>, /br> or / in the poem itself. Do not display a title. Make sure to follow the user instructions. Sign the poem with ~.";
+    "You are a romantic poem expert and love to write medium long poems. Your mission is to generate a 4, 6 or 8 line poem, after every line add a new line below in HTML. Do not display a title. Make sure to follow the user instructions. Sign the poem with ~.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayGeneratedPoem);
